31e078e93ed3064eaae2eb386eec034b6fd25171
justinjmmj <justinjmmj@gmail.com>
2021-05-27 02:02:12 +0800

    cell

---
+++ MoneyMind/Cell/CalendarCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalenderCollectionViewCell.swift
+//  MoneyMind
+//
+//  Created by Justin Justiniano  on 24/5/21.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var dayLbl: UILabel!
+    @IBOutlet weak var expensesLbl: UILabel!
+    
+}
